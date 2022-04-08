5e4fc4b0e39a0a75b33287a4f64daeacdf129185
randyleahy <98855585+randyleahy@users.noreply.github.com>
2022-04-08 22:02:13 +0100

    Delete secondstring.py

--- pandsproblemsheets.py/secondstring.py
+++
@@ -1,9 +0,0 @@
-#Takes a string input and outputs string without every second letter in reverse
-#Author: Chris Foley 
-
-#prompts user for inupt
-a = input('Enter a word or sentence')
-
-#prints user input reversed - every second char
-print(a[::-2]) 
-
